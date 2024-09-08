b6713d4e2a6c46fb2ec3f704638b97b6576aee34
Matthew Perez <ma617876@ucf.edu>
2024-09-08 20:30:27 +0000

    Changed calDistance

--- fracturing.c
+++ fracturing.c
@@ -18,32 +18,24 @@
 // defining PI as 3.14159 so that I can use 'PI' instead of having to write out 3.14159 every time.
 #define PI 3.14159
 
-// Declaring all the functions I will be using.
-// double calculateDistance();
-// double calculatePerimeter();
-// double calculateArea();
-// double calculateWidth();
-// double calculateHeight();
-
-// int main is where I will call all the functions that I created.
-
-
 //The function that will take two coordinates as an input and calculate the distance between the two points
 double calculateDistance() {
-    double x1, y1, x2, y2, distance;
+    static double distance = 0;
     static int counter = 0;
 //Using 'scanf' so that I can take in the variables x1, y1, x2, and y2 and assigning the variabnles to values
+    if(counter == 0){
+    double x1, y1, x2, y2;
     scanf("%lf %lf", &x1, &x2);
-
     scanf("%lf %lf", &y1, &y2);
     
     printf("Point #1 entered: x1 = %.2lf; y1 = %.2lf\n", x1, y1);
     printf("Point #2 entered: x2 = %.2lf; y2 = %.2lf\n", x2, y2); 
 
-//Using the distance formula to calculate distance using the points.
+    //Using the distance formula to calculate distance using the points.
     distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-    if(counter == 0){
     printf("The distance between the two points is %.3lf\n", distance);
+   
+   
     counter++;
     }
 
@@ -90,6 +82,8 @@ double calculateHeight() {
 
     return 3.0;  
 }
+
+// int main is where I will call all the functions that I created.
 int main(int argc, char **argv) {
     calculateDistance();
     calculatePerimeter();
